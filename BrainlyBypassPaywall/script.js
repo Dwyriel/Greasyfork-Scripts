ab2b3dd1086a8b2ae7202c9d97d3d673a6cbf4a9
Dwyriel <dwyriel333@hotmail.com>
2023-07-07 17:20:18 -0300

    Updated brainly's urls

--- BrainlyBypassPaywall/script.js
+++ BrainlyBypassPaywall/script.js
@@ -1,19 +1,21 @@
 // ==UserScript==
 // @name         BrainlyBypassPaywall
 // @namespace    https://github.com/Dwyriel
-// @version      1.3
+// @version      1.3.1
 // @description  Clears brainly's local storage to reset daily count to bypass the paywall
 // @author       Dwyriel
 // @license      MIT
-// @match        *://*brainly.in/*
-// @match        *://*brainly.com/*
-// @match        *://*brainly.it/*
-// @match        *://*brainly.co.id/*
-// @match        *://*brainly.ro/*
-// @match        *://*brainly.ph/*
-// @match        *://*brainly.lat/*
 // @match        *://*brainly.pl/*
+// @match        *://*znanija.com/*
+// @match        *://*brainly.lat/*
 // @match        *://*brainly.com.br/*
+// @match        *://*nosdevoirs.fr/*
+// @match        *://*eodev.com/*
+// @match        *://*brainly.ro/*
+// @match        *://*brainly.co.id/*
+// @match        *://*brainly.in/*
+// @match        *://*brainly.ph/*
+// @match        *://*brainly.com/*
 // @grant        none
 // @run-at       document-start
 // @homepageURL  https://github.com/Dwyriel/Greasyfork-Scripts
