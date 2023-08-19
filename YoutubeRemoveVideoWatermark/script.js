6224f4e3bdb8451fd0e7fddc307b142f26ee51c2
Dwyriel <dwyriel333@hotmail.com>
2023-08-19 10:30:10 -0300

    YT RemoveWatermark Optimization
    Removing some useless code as it doesn't seem like they'll be changing how it works for good while.

--- YoutubeRemoveVideoWatermark/script.js
+++ YoutubeRemoveVideoWatermark/script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Remove Video Watermark
 // @namespace    https://github.com/Dwyriel
-// @version      1.1
+// @version      1.2
 // @description  Simply removes the branding watermark from some videos, for slightly better video visibility.
 // @author       Dwyriel
 // @license      MIT
@@ -12,24 +12,11 @@
 
 (function () {
     'use strict';
-    const elementsByIdToRemove = [];
-    const elementsByNameToRemove = [];
-    const elementsByClassToRemove = [
-        "branding-img" // the image inside the button, removing this makes the button have a size of 0x0, keeping its functionality to avoid errors but essencially removing it.
-    ];
+    const elementByClassToRemove = "branding-img"; // the image inside the button, removing this makes the button have a size of 0x0, keeping its functionality to avoid errors but essentially removing it.
     const callback = () => {
-        for (let id of elementsByIdToRemove)
-            document.getElementById(id)?.remove();
-        for (let elementName of elementsByNameToRemove) {
-            let elements = document.getElementsByTagName(elementName);
-            for (let element of elements)
-                element.remove();
-        }
-        for (let className of elementsByClassToRemove){
-            let elements = document.getElementsByClassName(className);
-            for (let element of elements)
-                element.remove();
-        }
+        let elements = document.getElementsByClassName(elementByClassToRemove);
+        for (let element of elements)
+            element.remove();
     };
     const config = { attributes: true, childList: true, subtree: true };
     new MutationObserver(callback).observe(document.body, config);
